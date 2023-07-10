52c58f54492a5169a954e02deb34ebb769ca86e9
Anthony <apham.dev@gmail.com>
2023-07-10 16:16:38 -0700

    writing first jest test

---
+++ testingJest.js
@@ -0,0 +1,10 @@
+// JEST
+
+function concatStrings(strA, strB) {
+    return strA + strB;
+}
+
+expect(concatStrings("123","456")).toBe("123456");
+
+
+// we would also need to make sure we install jest globally
